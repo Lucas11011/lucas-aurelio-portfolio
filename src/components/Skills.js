f81b7624dad974a0b733c694ea922b64b815c2a0
Lucas11011 <lnaurelio219@gmail.com>
2026-02-18 14:41:09 -0500

    skills update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,22 +12,30 @@ import javaIcon from '../icons/Java.svg';
 import htmlIcon from '../icons/HTML5.svg';
 import cssIcon from '../icons/CSS3.svg';
 import reactIcon from '../icons/React.svg';
-import bootstrapIcon from '../icons/Bootstrap.svg';
+// import bootstrapIcon from '../icons/Bootstrap.svg';
 import tailwindIcon from '../icons/Tailwind CSS.svg';
+import nodeIcon from '../icons/Node.js.svg';
+import mysqlIcon from '../icons/MySQL.svg';
 // import vscodeIcon from '../icons/Visual Studio Code (VS Code).svg';
 // import filezillaIcon from '../icons/FileZilla.svg';
 import figmaIcon from '../icons/Figma.svg';
 import gitIcon from '../icons/Git.svg';
 import githubIcon from '../icons/GitHub.svg';
 import vercelIcon from '../icons/Vercel.svg';
+// import photoshopIcon from '../icons/Adobe Photoshop.svg';
+// import illustratorIcon from '../icons/Adobe Illustrator.svg';
 
 
 function Skills() {
     return (
         <div className="skills-content">
             <div className="skills-category">
-                <h3 className="category-label">Programming:</h3>
+                <h3 className="category-label">Frontend</h3>
                 <div className="skills-badges">
+                    <span className="skill-badge react">
+                        <img src={reactIcon} alt="React" className="skill-icon" />
+                        <span className="skill-name">React</span>
+                    </span>
                     <span className="skill-badge javascript">
                         <img src={jsIcon} alt="JavaScript" className="skill-icon" />
                         <span className="skill-name">JavaScript</span>
@@ -36,6 +44,42 @@ function Skills() {
                         <img src={tsIcon} alt="TypeScript" className="skill-icon" />
                         <span className="skill-name">TypeScript</span>
                     </span>
+                    <span className="skill-badge html">
+                        <img src={htmlIcon} alt="HTML5" className="skill-icon" />
+                        <span className="skill-name">HTML5</span>
+                    </span>
+                    <span className="skill-badge css">
+                        <img src={cssIcon} alt="CSS3" className="skill-icon" />
+                        <span className="skill-name">CSS3</span>
+                    </span>
+                    <span className="skill-badge tailwind">
+                        <img src={tailwindIcon} alt="Tailwind CSS" className="skill-icon" />
+                        <span className="skill-name">Tailwind  CSS</span>
+                    </span>
+                    {/* <span className="skill-badge bootstrap">
+                        <img src={bootstrapIcon} alt="Bootstrap" className="skill-icon" />
+                        <span className="skill-name">Bootstrap</span>
+                    </span> */}
+                </div>
+            </div>
+
+            <div className="skills-category">
+                <h3 className="category-label">Backend</h3>
+                <div className="skills-badges">
+                    <span className="skill-badge nodejs">
+                        <img src={nodeIcon} alt="Node.js" className="skill-icon" />
+                        <span className="skill-name">Node.js</span>
+                    </span>
+                    <span className="skill-badge sql">
+                        <img src={mysqlIcon} alt="SQL" className="skill-icon" />
+                        <span className="skill-name">SQL</span>
+                    </span>
+                </div>
+            </div>
+
+            <div className="skills-category">
+                <h3 className="category-label">Programming</h3>
+                <div className="skills-badges">
                     <span className="skill-badge cpp">
                         <img src={cppIcon} alt="C++" className="skill-icon" />
                         <span className="skill-name">C++</span>
@@ -56,39 +100,7 @@ function Skills() {
             </div>
 
             <div className="skills-category">
-                <h3 className="category-label">Web:</h3>
-                <div className="skills-badges">
-                    <span className="skill-badge html">
-                        <img src={htmlIcon} alt="HTML5" className="skill-icon" />
-                        <span className="skill-name">HTML5</span>
-                    </span>
-                    <span className="skill-badge css">
-                        <img src={cssIcon} alt="CSS3" className="skill-icon" />
-                        <span className="skill-name">CSS3</span>
-                    </span>
-                </div>
-            </div>
-
-            <div className="skills-category">
-                <h3 className="category-label">Frameworks:</h3>
-                <div className="skills-badges">
-                    <span className="skill-badge react">
-                        <img src={reactIcon} alt="React" className="skill-icon" />
-                        <span className="skill-name">React</span>
-                    </span>
-                    <span className="skill-badge bootstrap">
-                        <img src={bootstrapIcon} alt="Bootstrap" className="skill-icon" />
-                        <span className="skill-name">Bootstrap</span>
-                    </span>
-                    <span className="skill-badge tailwind">
-                        <img src={tailwindIcon} alt="Tailwind CSS" className="skill-icon" />
-                        <span className="skill-name">Tailwind CSS</span>
-                    </span>
-                </div>
-            </div>
-
-            <div className="skills-category">
-                <h3 className="category-label">Tech:</h3>
+                <h3 className="category-label">Tools</h3>
                 <div className="skills-badges">
                     <span className="skill-badge figma">
                         <img src={figmaIcon} alt="Figma" className="skill-icon" />
@@ -106,6 +118,14 @@ function Skills() {
                         <img src={vercelIcon} alt="Vercel" className="skill-icon" />
                         <span className="skill-name">Vercel</span>
                     </span>
+                    {/* <span className="skill-badge photoshop">
+                        <img src={photoshopIcon} alt="Photoshop" className="skill-icon" />
+                        <span className="skill-name">Photoshop</span>
+                    </span> */}
+                    {/* <span className="skill-badge illustrator">
+                        <img src={illustratorIcon} alt="Illustrator" className="skill-icon" />
+                        <span className="skill-name">Illustrator</span>
+                    </span> */}
                     {/* <span className="skill-badge vscode">
                         <img src={vscodeIcon} alt="VS Code" className="skill-icon" />
                         <span className="skill-name">VS Code</span>
